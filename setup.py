90534420ab836656a2487a036a288cd48a76b515
sterbini <guido.sterbini@cern.ch>
2018-03-27 14:47:38 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cl2pd',
+      version='0.01',
+      description='TODO',
+      url='https://github.com/sterbini/cl2pd',
+      author='Guido STERBINI',
+      author_email='guido.sterbini@cern.ch',
+      license='MIT',
+      packages=['cl2pd'],
+      zip_safe=False)
