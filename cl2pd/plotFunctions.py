4d9aec3a488c930c37a0bd4adacffac9050929d8
sterbini <guido.sterbini@cern.ch>
2018-04-13 18:58:52 +0200

    Update plotFunctions.py

--- cl2pd/plotFunctions.py
+++ cl2pd/plotFunctions.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 
 def setSourcePlot(gca, pltDescription,x=0.99,y=0.01,horizontalalignment='right',\
                   color='lightgray',verticalalignment='bottom',rotation=0,fontsize=7):
-   '''
-   Comment the plot adding for instance the source script.
+  '''
+  Comment the plot adding for instance the source script.
    
-   It requires an axis (gca) and a string (pltDescription).
+  It requires an axis (gca) and a string (pltDescription).
    
-   ===EXAMPLE===
-   plt.plot([1,2,3,])
-   setSourcePlot(plt.gca(), pltDescription,x=0.99,y=0.01,horizontalalignment='right',\
+  ===EXAMPLE===
+  plt.plot([1,2,3,])
+  setSourcePlot(plt.gca(), pltDescription,x=0.99,y=0.01,horizontalalignment='right',\
                   color='lightgray',verticalalignment='bottom',rotation=0,fontsize=7)
-   '''
-    plt.text(x,y,pltDescription, 
+  '''
+  plt.text(x,y,pltDescription, 
              horizontalalignment=horizontalalignment, 
              color=color,
              verticalalignment=verticalalignment, 
