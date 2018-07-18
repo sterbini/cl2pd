10d521499a3b492f686fc22a8bb35bb984b397e9
Suad <skrilasevi1@etf.unsa.ba>
2018-07-18 17:04:51 +0200

    Added missing imports

--- cl2pd/importData.py
+++ cl2pd/importData.py
@@ -5,6 +5,7 @@ import inspect
 # Fundamental contribution by R. De Maria et al.
 import pytimber
 import dotdict
+dotdict=dotdict.dotdict
 
 # TODO: discuss about the possible problem if the user has already defined a variable named 'cals' 
 cals=pytimber.LoggingDB()
