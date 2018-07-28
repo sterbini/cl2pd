9cab79d6da3cea53fc256f5354a5b2e2165f8c4f
sterbini <guido.sterbini@cern.ch>
2018-07-28 17:00:33 +0200

    Update plotFunctions.py

--- cl2pd/plotFunctions.py
+++ cl2pd/plotFunctions.py
@@ -68,116 +68,117 @@ def setShadedRegion(ax,color='g' ,xLimit=[0,1],alpha=.1):
     ax.set_ylim(aux)
     
 def plotLHCFill(myFill,myTitle,startTime):
-        """
-        It returns the plot of the beam modes of the pd DF.
-         
-        ===Example===
-        myFill=importData.LHCFillsByNumber([6797,6798,6799])
-        plotFunctions.plotLHCFill(myFill,myTitle="This is my title",startTime=pd.Timestamp('2018-06-14 19:39:39.435000'))
-        """ 
-        aux=myFill[myFill['mode']=='FILL']
-        b=myFill[myFill['mode']!='FILL']
-        def colorMe(i):
-            if np.mod(i,2):
-                return "m"
-            else:
-                return "c"
+    """
+    It returns the plot of the beam modes of the pd DF.
 
-        for i in aux.index:
-            plt.gca().fill_between([pd.Timestamp(aux[aux.index==i]['startTime'].values[0]).to_pydatetime(),
-                                    pd.Timestamp(aux[aux.index==i]['endTime'].values[0]).to_pydatetime()], [0,0], [2.1,2.1],color=colorMe(i), alpha=.1)
+    ===Example===
+    myFill=importData.LHCFillsByNumber([6797,6798,6799])
+    plotFunctions.plotLHCFill(myFill,myTitle="This is my title",startTime=pd.Timestamp('2018-06-14 19:39:39.435000'))
+    """ 
+    aux=myFill[myFill['mode']=='FILL']
+    b=myFill[myFill['mode']!='FILL']
+    def colorMe(i):
+        if np.mod(i,2):
+            return "m"
+        else:
+            return "c"
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='NOBEAM']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [.8,.8], [.9,.9],color='r')
+    for i in aux.index:
+        plt.gca().fill_between([pd.Timestamp(aux[aux.index==i]['startTime'].values[0]).to_pydatetime(),
+                                pd.Timestamp(aux[aux.index==i]['endTime'].values[0]).to_pydatetime()], [0,0], [2.1,2.1],color=colorMe(i), alpha=.1)
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='CYCLING']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [.9,.9], [1,1],color='r')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='NOBEAM']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [.8,.8], [.9,.9],color='r')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='SETUP']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1,1], [1.1,1.1],color='r')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='CYCLING']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [.9,.9], [1,1],color='r')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='INJPROT']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.1,1.1], [1.2,1.2],color='k')
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='INJPHYS']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.2,1.2], [1.3,1.3],color='orange')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='SETUP']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1,1], [1.1,1.1],color='r')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='PRERAMP']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.3,1.3], [1.4,1.4],color='b')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='INJPROT']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.1,1.1], [1.2,1.2],color='k')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='INJPHYS']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.2,1.2], [1.3,1.3],color='orange')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='RAMP']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.4,1.4], [1.5,1.5],color='b')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='PRERAMP']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.3,1.3], [1.4,1.4],color='b')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='FLATTOP']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.5,1.5], [1.6,1.6],color='b')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='RAMP']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.4,1.4], [1.5,1.5],color='b')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='SQUEEZE']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.6,1.6], [1.7,1.7],color='g')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='FLATTOP']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.5,1.5], [1.6,1.6],color='b')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='ADJUST']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.7,1.7], [1.8,1.8],color='g')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='SQUEEZE']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.6,1.6], [1.7,1.7],color='g')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='STABLE']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.8,1.8], [1.9,1.9],color='g')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='ADJUST']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.7,1.7], [1.8,1.8],color='g')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='BEAMDUMP']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.9,1.9], [2,2],color='k')
-                plt.plot(pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),1.95,'vr',ms=12)
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='STABLE']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.8,1.8], [1.9,1.9],color='g')
 
-            bb=b[b.index==i]
-            bbb=bb[bb['mode']=='RAMPDOWN']
-            if len(bbb): 
-                plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
-                                        pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [2,2], [2.1,2.1],color='k')
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='BEAMDUMP']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [1.9,1.9], [2,2],color='k')
+            plt.plot(pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),1.95,'vr',ms=12)
+
+        bb=b[b.index==i]
+        bbb=bb[bb['mode']=='RAMPDOWN']
+        if len(bbb): 
+            plt.gca().fill_between([pd.Timestamp(bbb['startTime'].values[0]).to_pydatetime(),
+                                    pd.Timestamp(bbb['endTime'].values[0]).to_pydatetime()], [2,2], [2.1,2.1],color='k')
 
 
-            myTest=aux[aux.index==i]['duration']> pd.Timedelta(hours=.3)
-            if myTest.values[0]:
-                myDiff=(pd.to_datetime(aux[aux.index==i]['endTime'].values[0])-pd.to_datetime(aux[aux.index==i]['startTime'].values[0]))/2
-                myTime=pd.to_datetime(aux[aux.index==i]['startTime'].values[0])+myDiff
-                plt.text(myTime,.6 ,str(i), bbox=dict(facecolor='w', alpha=1),horizontalalignment='center',verticalalignment='center', rotation=90)
-        plt.yticks([.85, .95, 1.05, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 1.75, 1.85, 1.95, 2.05],
-                   ['NOBEAM','CYCLING','SETUP','INJPROT','INJPHYS',
-                    'PRERAMP','RAMP','FLATTOP','SQUEEZE','ADJUST','STABLE','BEAMDUMP','RAMPDOWN']);
-        plt.ylim(0.4,2.1);
-        plt.title(myTitle)
-        plt.xticks(rotation=45)
-        plt.xlabel('UTC time [month-day hh]');
+        myTest=aux[aux.index==i]['duration']> pd.Timedelta(hours=.3)
+        if myTest.values[0]:
+            myDiff=(pd.to_datetime(aux[aux.index==i]['endTime'].values[0])-pd.to_datetime(aux[aux.index==i]['startTime'].values[0]))/2
+            myTime=pd.to_datetime(aux[aux.index==i]['startTime'].values[0])+myDiff
+            plt.text(myTime,.6 ,str(i), bbox=dict(facecolor='w', alpha=1),horizontalalignment='center',verticalalignment='center', rotation=90)
+    plt.yticks([.85, .95, 1.05, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 1.75, 1.85, 1.95, 2.05],
+               ['NOBEAM','CYCLING','SETUP','INJPROT','INJPHYS',
+                'PRERAMP','RAMP','FLATTOP','SQUEEZE','ADJUST','STABLE','BEAMDUMP','RAMPDOWN']);
+    plt.ylim(0.4,2.1);
+    plt.title(myTitle)
+    plt.xticks(rotation=45)
+    plt.xlabel('UTC time [month-day hh]');
+    return plt.gca()
 
 def shadedDF(gca, df, color='g', alpha=0.5):
     '''
@@ -237,7 +238,8 @@ def plot_BBMatrix(BBMatrixLHC, B1_bunches, B2_bunches,alpha=.2, width=1):
     setArrowLabel(ax=plt.gca(),label='IP8',labelPosition=(2000,2900), arrowPosition=(2000,2900), myColor='k')
     setArrowLabel(ax=plt.gca(),label='IP8',labelPosition=(3130,3564-3130), arrowPosition=(3130,3564-3130), myColor='k')
     setArrowLabel(ax=plt.gca(),label='IP2',labelPosition=(3564-3130,3130), arrowPosition=(3564-3130,3130), myColor='k');
-    
+    return plt.gca()
+
 def plotBBEncounterSchedule(BBEncounterSchedule,beam,bunch,exp):
     '''
     Plot the encounter schedule for a specific beam, bunch, and experiment.
@@ -269,6 +271,5 @@ def plotBBEncounterSchedule(BBEncounterSchedule,beam,bunch,exp):
         else:
             setArrowLabel(plt.gca(), label=str(encounters[0]), arrowPosition=(x[0], y[0]), labelPosition=(x[0]-5, y[0]+.01), myColor='b', arrowArc_rad=-0.2)
             setArrowLabel(plt.gca(), label=str(encounters[-1]), arrowPosition=(x[-1], y[-1]), labelPosition=(x[-1]+5, y[-1]-.01), myColor='r', arrowArc_rad=-0.2)
-    ax1=plt.gca()
     plt.grid(ls=':')
-    return ax1 
+    return plt.gca()
