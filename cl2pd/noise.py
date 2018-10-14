b3a65d7ca21233ec31830ac9f693a7b907d5fd8c
sterbini <guido.sterbini@cern.ch>
2018-10-14 12:06:22 +0200

    Update noise.py

--- cl2pd/noise.py
+++ cl2pd/noise.py
@@ -1,6 +1,6 @@
-'''
+"""
 In this module we will put the functions needed for the noise analysis
-'''
+"""
 import numpy as np
 import pandas as pd
 import glob  
@@ -8,7 +8,7 @@ import os
 import doctdict
 
 def fromName2Timestamp(myString,tz_start='CET',tz_end='UTC'):
-    '''
+    """
     Convert a file path in a pd.Timestamp. 
     tz_start: the initial tz
     tz_end: the tz of the returned pd.Timestamp.
@@ -16,17 +16,21 @@ def fromName2Timestamp(myString,tz_start='CET',tz_end='UTC'):
     EXAMPLE:
     myString='/eos/project/a/abpdata/lhc/rawdata/power_converter/05Oct2018_21_15_36_current'
     fromName2Timestamp(myString)
-    '''
+    """
     [a,b,c,d,e]=myString.split('/')[-1].split('_')
     aa=a[:2]+'-'+a[2:5] + '-'+a[5:9]+' '
     return pd.Timestamp(aa + b+':' + c+':'+ d).tz_localize(tz_start).tz_convert(tz_end)
     
-def importEmptyDF(dir_name):
-  myDATA=dotdict.dotdict()
-  for i in ['current','voltage']:
-      myFileList=glob.glob(dir_name+'/*'+i)
+def importEmptyDF(folderName):
+    """
+    Import the list of the EPC acquisition stored in folderName.
+    """
+    myDATA=dotdict.dotdict()
+    for i in ['current','voltage']:
+      myFileList=glob.glob(folderName+'/*'+i)
       myTimestampList=[]
       for fileName in myFileList:
           myTimestampList.append(fromName2Timestamp(fileName))
       myDATA['at'+i]=pd.DataFrame(index=myTimestampList)
       myDATA['at'+i]['fileName']=myFileList
+    return myDATA
